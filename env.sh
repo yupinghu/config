6cf23890cc7d85600255e2398dc270885803f6b3
Yu Ping Hu <yu.ping.hu@gmail.com>
2017-08-28 16:25:12 -0700

    Add alias for grepping whole project.

--- env.sh
+++ env.sh
@@ -19,6 +19,8 @@ if [ -z ${USERNAME+x} ]; then
     export USERNAME=$USER
 fi
 
+alias grp='grep -r -I --exclude-dir=build --exclude-dir=cmake-build* --exclude-dir=obj --exclude-dir=dst'
+
 # basic aliases
 alias ll='ls -alGF'
 alias la='ls -AGF'
