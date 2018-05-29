dbbf40f0a155b129d0dbe8b3dcd7012db721178a
Yu Ping Hu <yu.ping.hu@gmail.com>
2018-05-29 01:36:14 -0700

    Improved WSL aliases.
    Also suppress dictionary errors in atom.

--- env-more-stuff.sh
+++ env-more-stuff.sh
@@ -42,32 +42,31 @@ alias spectacle='run Spectacle'
 alias xcode='run Xcode'
 
 # WSL: Application aliases
-function atom() {
-  (cd "$1" && cmd.exe /c atom .)
-}
 
 # Note that you can also use this to open folders.
 alias start="cmd.exe /c start"
 
-# Start apps where they live.
-runInDir() {
-  (cd "$1/$2" && shift && shift && start $@)
-}
-runProgram() {
-  runInDir "/mnt/c/Program Files" "$@"
-}
-runX86Program() {
-  runInDir "/mnt/c/Program Files (x86)/" "$@"
-}
-runLocalApp() {
-  runInDir "/mnt/c/Users/yupin/AppData/Local" "$@"
+function atom() {
+  (cd "$1" && cmd.exe /c atom .)
 }
 
-alias bnet='runX86Program Battle.net Battle.net Launcher.exe'
-alias calibre='runProgram Calibre2 calibre.exe'
-alias chrome='runX86Program Google/Chrome/Application chrome.exe'
-alias discord='runLocalApp Discord Update.exe --processStart Discord.exe'
-alias kobo='runX86Program Kobo Kobo.exe'
-alias signal='runLocalApp Programs/signal-desktop Signal.exe'
-alias steam='runX86Program Steam Steam.exe'
-alias as='runProgram "Android/Android Studio/bin" studio64.exe'
+# startApp path-to-app exeFileName [params...]
+function startApp() {
+  start /d "`wslpath -w "$1"`" "${@:2}"
+}
+
+# Common locations for use with startApp.
+PROGRAM_FILES_PATH='/mnt/c/Program Files'
+PROGRAM_FILES_X86_PATH='/mnt/c/Program Files (x86)'
+START_MENU_PATH='/mnt/c/ProgramData/Microsoft/Windows/Start Menu/Programs'
+LOCAL_APP_PATH='/mnt/c/Users/yupin/AppData/Local'
+
+# Battle.net is weird because the exe name has a space in it, so just run the start menu shortcut.
+alias bnet='startApp "$START_MENU_PATH/Battle.net" Battle.net.lnk'
+alias calibre='startApp "$PROGRAM_FILES_PATH/Calibre2" calibre.exe'
+alias chrome='startApp "$PROGRAM_FILES_X86_PATH/Google/Chrome/Application" chrome.exe'
+alias discord='startApp "$LOCAL_APP_PATH/Discord" Update.exe --processStart Discord.exe'
+alias kobo='startApp "$PROGRAM_FILES_X86_PATH/Kobo" Kobo.exe'
+alias signal='startApp "$LOCAL_APP_PATH/Programs/signal-desktop" Signal.exe'
+alias steam='startApp "$PROGRAM_FILES_X86_PATH/Steam" Steam.exe'
+alias as='startApp "$PROGRAM_FILES_PATH/Android/Android Studio/bin" studio64.exe'
