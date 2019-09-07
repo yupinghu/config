7f6eb4d57b1b144badb6b9c3d24821274060e29a
Yu Ping Hu <yu.ping.hu@gmail.com>
2019-09-07 11:47:43 -0700

    Fix some of the chocolatey installs.

--- install-software-windows.bat
+++ install-software-windows.bat
@@ -1,28 +1,25 @@
 @echo off
 
 rem Install chocolatey
-
 powershell -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 rem Confirm chocolatey install
-
 pause
 
 rem Install stuff
-
 choco install googlechrome
 choco install atom
 choco install hackfont
 choco install steam
 choco install discord.install
-choco install sharpkeys
 
-rem battle.net installer isn't in chocolatey
-
-powershell (new-object System.Net.WebClient).DownloadFile('https://www.battle.net/download/getInstallerForGame?os=win^&locale=enUS^&version=LIVE^&gameProgram=BATTLENET_APP','C:\home\yph\tmp\Battle.net-Setup.exe')
+rem These aren't available on chocolatey.
+rem https://www.randyrants.com/category/sharpkeys/
+rem https://www.blizzard.com/en-us/apps/battle.net/desktop
+pause
 
 rem Install WSL & Ubuntu -- do this last in case it fails
-
 choco install wsl
-choco install ubuntu-1804
 choco install wsltty
+rem probably need to reboot before the next line will work
+rem choco install ubuntu-1804
