85e7c63b986c1259c91f8c45c2f502e32729775e
Yu Ping Hu <yu.ping.hu@gmail.com>
2023-02-13 16:17:58 -0800

    zsh configs.

--- env.sh
+++ env.sh
@@ -28,9 +28,14 @@ if [[ `uname` == 'Darwin' ]]; then
   alias la='ls -AGF'
   alias l='ls -CGF'
 else
+  alias ls='ls --color=auto'
   alias ll='ls -alFv --color=auto'
   alias la='ls -AF --color=auto'
   alias l='ls -CF --color=auto'
+
+  alias grep='grep --color=auto'
+  alias fgrep='fgrep --color=auto'
+  alias egrep='egrep --color=auto'
 fi
 alias tidy='rm -f *~ .*~'
 
@@ -77,63 +82,42 @@ gcpt() {
 # Git prompt: defaults are blue hostname. WSL uses this as-is; Macbook changes hostname; remote
 # hosts set red prompt and might override name.
 PROMPT_HOST=$(hostname)
-PROMPT_HEADER_COLOR="34"
-if [ "$(uname)" == "Darwin" ]; then
+if [[ "$(uname)" == "Darwin" ]]; then
   # Macbook doesn't want to use whatever random hostname it has.
   PROMPT_HOST="macbook"
-elif [[ -z ${WSL_DISTRO_NAME}+x ]]; then
-  # Non-WSL/Mac (i.e. remote) hosts get a red prompt.
-  PROMPT_HEADER_COLOR="33"
 fi
-. ~/config/git-prompt.sh
-GIT_PS1_DESCRIBE_STYLE='describe'
-GIT_PS1_SHOWDIRTYSTATE=1
-GIT_PS1_SHOWCOLORHINTS=1
-PROMPT_BODY='\[\033[${PROMPT_HEADER_COLOR}m\]\u@${PROMPT_HOST}\[\033[0m\]:\[\033[35m\]\w\[\033[0m\]'
-GIT_PROMPT_COMMAND='__git_ps1 "$PROMPT_BODY" "\\\$ "'
-UNGIT_PROMPT="${PROMPT_BODY}\$ "
-alias setgitprompt='export PROMPT_COMMAND=$GIT_PROMPT_COMMAND'
-alias unsetgitprompt='unset PROMPT_COMMAND && PS1=$UNGIT_PROMPT'
-# Default turn git prompt on.
-setgitprompt
 
-# bash_completion
-if ! shopt -oq posix; then
-  if [ -f /usr/share/bash-completion/bash_completion ]; then
-    . /usr/share/bash-completion/bash_completion
-  elif [ -f /etc/bash_completion ]; then
-    . /etc/bash_completion
-  fi
+if [ -z ${ZSH_NAME+x} ]; then
+  . ~/config/git-prompt.sh
+  GIT_PS1_DESCRIBE_STYLE='describe'
+  GIT_PS1_SHOWDIRTYSTATE=1
+  GIT_PS1_SHOWCOLORHINTS=1
+  PROMPT_BODY='\[\033[34m\]\u@${PROMPT_HOST}\[\033[0m\]:\[\033[35m\]\w\[\033[0m\]'
+  GIT_PROMPT_COMMAND='__git_ps1 "$PROMPT_BODY" "\\\$ "'
+  UNGIT_PROMPT="${PROMPT_BODY}\$ "
+  export PROMPT_COMMAND=$GIT_PROMPT_COMMAND
+else
+  autoload -Uz add-zsh-hook vcs_info
+  setopt prompt_subst
+  add-zsh-hook precmd vcs_info
+  zstyle ':vcs_info:*' check-for-changes true
+  zstyle ':vcs_info:*' unstagedstr ' %F{red}*%f'
+  zstyle ':vcs_info:*' stagedstr ' %F{green}+%f'
+  zstyle ':vcs_info:git:*' formats       ' (%F{green}%b%f%u%c)'
+  zstyle ':vcs_info:git:*' actionformats ' (%b|%a%u%c)'
+
+  PROMPT='%F{blue}%n@${PROMPT_HOST}%f:%F{magenta}%~%f${vcs_info_msg_0_}%# '
 fi
 
 test -r .dircolors && eval "$(dircolors .dircolors)"
-
-NC='\033[0m'
-
-function infmsg() {
-  echo -e "\033[0;32m *** $1${NC}"
-}
-
-function wrnmsg() {
-  echo -e "\033[0;33m *** $1${NC}"
-}
-
-function errmsg() {
-  echo -e "\033[0;31m *** $1${NC}"
-}
 
 alias rmtmp='rm -rf ~/tmp/*'
 
 alias scp='scp -A'
 alias ssh='ssh -A'
 
-alias wipe='clear && history -c'
-alias cls='clear && history -c'
-
-# Useful for Macbook network problems.
-if [ "$(uname)" == "Darwin" ]; then
-  alias flushnetwork='sudo ifconfig en0 down && sleep 5 && sudo route flush && sudo ifconfig en0 up'
-fi
+alias wipe='clear && history -p'
+alias cls='clear && history -p'
 
 # Go stuff
 if command -v go &> /dev/null; then
